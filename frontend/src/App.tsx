695c825605fcaeae4fa8c7edc08e897b9519d461
kyle-heller <kyle.heller@gmail.com>
2025-11-08 10:22:41 -0500

    react frontend scaffold

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'                                                           
+  import Dashboard from './pages/Dashboard'                                                                                 
+  import MonitorDetail from './pages/MonitorDetail'                                                                         
+  import './App.css'                                                                                                        
+                                                                                                                            
+  function App() {                                                                                                          
+    return (                                                                                                                
+      <BrowserRouter>                                                                                                       
+        <Routes>                                                                                                            
+          <Route path="/" element={<Dashboard />} />                                                                        
+          <Route path="/monitors/:id" element={<MonitorDetail />} />                                                        
+        </Routes>                                                                                                           
+      </BrowserRouter>                                                                                                      
+    )                                                                                                                       
+  }                                                                                                                         
+                                                                                                                            
+  export default App
