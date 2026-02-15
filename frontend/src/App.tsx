b050ffe8d564e164f95610fc448f6caa5082c74b
kyle-heller <kyle.heller@gmail.com>
2026-02-15 18:02:42 -0500

    track status codes in check metrics

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Dashboard from './pages/Dashboard'                                                                                 
 import MonitorDetail from './pages/MonitorDetail'                                                                         
 import AddMonitor from './pages/AddMonitor'                                                                               
-import './App.css'                                                                                                        
                                                                                                                           
 function App() {                                                                                                          
   return (                                                                                                                
